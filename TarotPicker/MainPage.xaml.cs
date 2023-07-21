82bbc2928c588d4123fcce3c4e61940cf3e68dab
Aaron Hawkins <aaron.hawkins@excellimatrix.com>
2023-07-21 15:00:23 -0600

    Add project files.

---
+++ TarotPicker/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Collections.ObjectModel;
+
+namespace TarotPicker
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly ObservableCollection<string> cardList = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            cardCollectionView.ItemsSource = cardList;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            string[] pickedCards = CardPicker.PickSomeCards((int)numberOfCards.Value);
+            cardList.Clear();
+            foreach (string card in pickedCards)
+            {
+                cardList.Add(card);
+            }
+        }
+    }
+}
---
+++ TarotPicker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TarotPicker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
